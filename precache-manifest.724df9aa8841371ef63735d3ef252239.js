2d70ee9928b8d4d37cc3b835c4f3e07b7ea35d5b
Arul Antony <Arul.Antony@agilysys.com>
2020-06-24 00:48:25 +0530

    Updates

---
+++ precache-manifest.724df9aa8841371ef63735d3ef252239.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fcd3b4ec7ccc732317f33118563da24",
+    "url": "/Mysearch/index.html"
+  },
+  {
+    "revision": "0fe3a9bd487a7a12d367",
+    "url": "/Mysearch/static/css/main.399edaf3.chunk.css"
+  },
+  {
+    "revision": "dc4ef47d2dd66864f0b5",
+    "url": "/Mysearch/static/js/2.23273add.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mysearch/static/js/2.23273add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe3a9bd487a7a12d367",
+    "url": "/Mysearch/static/js/main.87a1bddd.chunk.js"
+  },
+  {
+    "revision": "64085bbbf3e4fb329d1e",
+    "url": "/Mysearch/static/js/runtime-main.bc81fcbe.js"
+  },
+  {
+    "revision": "7684261aa96ad12771bd803013af1158",
+    "url": "/Mysearch/static/media/NoRecordFound.7684261a.png"
+  }
+]);
