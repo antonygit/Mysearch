2b771ca2538a7b410e5e9f990931ace3357ad6ed
Arul Antony <Arul.Antony@agilysys.com>
2020-06-24 00:40:41 +0530

    first commit

---
+++ book-store/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import  Form  from './components/Form';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <main className="app">
+      {/* <Form /> */}
+ 
+      <Search/>
+
+        
+    </main>
+  );
+}
+
+export default App;
